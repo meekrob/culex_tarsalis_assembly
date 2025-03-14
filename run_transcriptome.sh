96ba7b6378d92126fe4700d3389bd196b8941836
jake <jakelamb74@gmail.com>
2025-03-13 18:16:05 -0600

    .

--- run_transcriptome.sh
+++ run_transcriptome.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 snakemake \
-  --cluster "sbatch -p {resources.partition} -t {resources.time} -c {resources.cpus} --mem={resources.mem} -o logs/{rule}/{wildcards}.out -e logs/{rule}/{wildcards}.err" \
+  --executor slurm \
   --jobs 15 \
   --use-conda \
   --latency-wait 60 \
+  --default-resources partition=day-long-cpu time=24:00:00 mem=64G cpus=16 \
+  --slurm-logdir logs/ \
   results/transcriptome_assembly/06_busco/bbnorm/run_diptera_odb10/short_summary.txt \
   results/transcriptome_assembly/06_busco/trinity/run_diptera_odb10/short_summary.txt 
